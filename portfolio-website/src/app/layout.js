b290a4d05c9fd1e4dcff3e684cc237340b0e07af
Jacob Cabrera <jacobcabrera718@gmail.com>
2025-05-07 21:53:41 -0400

    background start threejs

--- portfolio-website/src/app/layout.js
+++ portfolio-website/src/app/layout.js
@@ -1,19 +1,12 @@
 import '../styles/globals.css';
-import Footer from '../components/footer';
-
-export const metadata = {
-  title: 'My Portfolio',
-  description: 'Created with Next.js and Tailwind CSS',
-};
+import background from '../components/background';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen flex-col">
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
+      <body>
+        <background />
+        {children}
       </body>
     </html>
   );
