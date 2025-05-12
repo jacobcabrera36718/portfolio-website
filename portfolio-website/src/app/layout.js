68ba18fe58b52c71b8b892fbe545276cac40a5f2
Jacob Cabrera <jacobcabrera718@gmail.com>
2025-05-12 00:09:55 -0400

    fixed issues

--- portfolio-website/src/app/layout.js
+++ portfolio-website/src/app/layout.js
@@ -1,13 +1,18 @@
 import '../styles/globals.css';
-import WaveBackground from '../components/Background/WaveBackground';
+import Footer from '../components/footer';
+
+export const metadata = {
+  title: 'My Portfolio',
+  description: 'Created by Jacob',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <WaveBackground />
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
