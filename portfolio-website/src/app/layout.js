2bf2d9cfb1fe157485f1eae6f4eccf4016713d19
Jacob Cabrera <jacobcabrera718@gmail.com>
2025-05-25 00:07:30 -0400

    fix

--- portfolio-website/src/app/layout.js
+++ portfolio-website/src/app/layout.js
@@ -1,5 +1,5 @@
 'use client';
-import '../styles/globals.css';
+import './globals.css';
 import Footer from '../components/footer';
 import ClientShaderWrapper from '../components/ClientShaderWrapper';
 import Preloader from '../components/Preloader';
